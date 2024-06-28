33897043adf0af4f1eeff1ca94c5ec57ed97bc6f
zkfakaii <zkfakaii@gmail.com>
2024-06-28 17:32:18 -0400

    V8

---
+++ Assets/Scripts/Hoyo.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hoyo : MonoBehaviour
+{
+    // Referencia al GameObject que representa el bache tapado
+    public GameObject bacheTapado;
+
+   
+    
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Empujable"))
+        {
+            // Desactivar el bache visible
+            Destroy(collision.gameObject);
+            Destroy(this.gameObject);
+        }
+
+    }
+
+}
+
+
+
+
+
+
